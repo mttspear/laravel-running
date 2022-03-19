c0ed96b3ca61eb1f7c856d11526e172e6eaa90d1
mttspear <mttspear@gmail.com>
2022-03-19 08:49:38 -0700

    working with comments

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -12,20 +12,26 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     <!-- Styles -->
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+        integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vivus/0.4.6/vivus.min.js"
+        integrity="sha512-oUUeA7VTcWBqUJD/VYCBB4VeIE0g1pg5aRMiSUOMGnNNeCLRS39OlkcyyeJ0hYx2h3zxmIWhyKiUXKkfZ5Wryg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
         body {
             font-family: 'Nunito', sans-serif;
+
         }
 
-        /*#space {
-            overflow: hidden;
+        .second-header {
             position: absolute;
-            top: 0;
+            bottom: 0;
             left: 0;
-            width: 100%;
-            height: 100%;
-        }*/
+        }
 
         #spaceCanvas {
             position: absolute;
@@ -38,6 +44,7 @@
             z-index: -10;
 
         }
+        /*
 
         #main {
             width: 100%;
@@ -85,7 +92,7 @@
         }
 
         body.homepage .transition-section {
-            min-height: 100vh;
+            min-height: 150vh;
             padding: 11% 0 18%;
         }
 
@@ -101,6 +108,7 @@
             bottom: 0;
             left: 0;
         }
+        
 
         #clouds {
             position: absolute;
@@ -108,6 +116,61 @@
             bottom: -14%;
             left: 0;
             width: 100%;
+        }
+        
+*/
+        #white-div {
+            min-height: 20%;
+        }
+
+
+        #clouds:after {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            /*width: 100%; */
+            height: 100px;
+            content: '';
+            background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, .82) 50%, white 100%);
+        }
+
+        .bottom-content {
+            /*padding: 40vh 0 122px;*/
+        }
+
+        
+
+        .gg {
+            display: flex;
+            height: 14px;
+            margin-bottom: 11px;
+        }
+
+        .gg-symbol {
+            height: 14px;
+            width: 14px;
+            background: #969696;
+            margin-right: 14px;
+        }
+
+        .gg-symbol--2 {
+            width: 56px;
+        }
+
+        .gg-symbol--3 {
+            width: 84px;
+        }
+
+        .gg-symbol--5 {
+            width: 140px;
+        }
+
+        .gg-symbol--8 {
+            width: 224px;
+        }
+
+        .gg-symbol--disc {
+            border-radius: 14px;
         }
 
     </style>
@@ -120,29 +183,77 @@
                 <div class="container star-container">
                     <div class="row">
                         <div class="col-xs-12">
-                            <p>hello top of page</p>
+                            <h1>OCJogger.com</h1>
                         </div>
+
+                        <div class="gg">
+                            <div class="gg-symbol gg-symbol--rect gg-symbol--5 gg-symbol--gradient"
+                                style="background: linear-gradient(90deg, rgb(255, 218, 122) 0%, rgb(255, 105, 105) 100%); opacity: 1; transform: translate(0px, 0px);">
+                            </div>
+                            <div class="gg-symbol gg-symbol--disc" style="opacity: 1; transform: translate(0px, 0px);">
+                            </div>
+                        </div>
+
+                        <div class="gg">
+                            <div class="gg-symbol gg-symbol--rect gg-symbol--8 gg-symbol--gradient"
+                                style="background: linear-gradient(90deg, rgb(242, 159, 255) 0%, rgb(124, 153, 255) 100%); opacity: 1; transform: translate(0px, 0px);">
+                            </div>
+                        </div>
+
+                        <div class="gg">
+                            <div class="gg-symbol gg-symbol--rect gg-symbol--3 gg-symbol--gradient"
+                                style="background: linear-gradient(90deg, rgb(100, 145, 214) 0%, rgb(67, 240, 199) 100%); opacity: 1; transform: translate(0px, 0px);">
+                            </div>
+                            <div class="gg-symbol gg-symbol--square"
+                                style="opacity: 1; transform: translate(0px, 0px);"></div>
+                            <div class="gg-symbol gg-symbol--rect gg-symbol--2"
+                                style="opacity: 1; transform: translate(0px, 0px);"></div>
+                            <div class="gg-symbol gg-symbol--rect gg-symbol--5 gg-symbol--gradient"
+                                style="background: linear-gradient(90deg, rgb(100, 145, 214) 0%, rgb(67, 240, 199) 100%); opacity: 1; transform: translate(0px, 0px);">
+                            </div>
+                        </div>
+
+
                     </div>
+
+                    <div class="row">
+                        <div class="second-header align-self-end text-end">
+                            <h2>App Development. Web Development. Python Analysis and Scripting</h2>
+                        </div>
+
+
+                    </div>
+
+
                 </div>
                 <div id="space"></div>
 
             </section>
             <section class="transition-section">
                 <div class="container">
-                    <div class="row">
-                        <h2>hello middle</h2>
+                    <div class="row text-center">
+                        <h2>Hand crafted digital solutions for your buisness needs</h2>
+                        <p>Experts in Laravel, SQL, </p>
                     </div>
                 </div>
-
                 <div id="clouds" class="container-row"></div>
-
-
             </section>
 
             <div class="container" id="white-div">
-                <div class="row">
-                    <div class="col-xs-12 text-center"></div>
+                <div class="row bottom-content">
+                    <div class="col-xs-12 text-center">
+                        <p>And the sky is the limits</p>
+                    </div>
+
                 </div>
+                <ul class="list">
+                    <li class="list-item one">https://github.com/mttspear</li>
+                    <li class="list-item two">https://www.upwork.com/freelancers/~01d673de10ec509b4a</li>
+                    <li class="list-item three">https://www.linkedin.com/in/matt-spear-b2949064/</li>
+                    <li class="list-item four">Laravel Sample link</li>
+                    <li class="list-item four">Running Analysis</li>
+                    <li class="list-item four">Resin Projects</li>
+                </ul>
 
             </div>
 
