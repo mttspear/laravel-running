1b4fd1d105f134bee18338cc17e5a3e737ab4901
Matt Spear <mttspear@gmail.com>
2024-07-31 21:42:21 +0000

    [~] update text

--- resources/views/projects/song.blade.php
+++ resources/views/projects/song.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', 'Laravel Song Application')
 
-@section('project-title', 'A Laravel application where users guess the songs')
+@section('project-title', 'Guess the Song: A PHP Music Game')
 
 @section('date', 'FEB. 20, 2023, AT 5:00 AM')
 
@@ -15,33 +15,44 @@
     <img alt="SongArt" src="{{ asset('img/song.jpg') }}">
 
     <hr>
-    <p>This Laravel application is a simple game where users go back and forth seeing who can name the most songs by a given
-        musical artist.</p>
+    <p>This PHP application is an engaging game where users compete to see who can name the most songs by a given musical artist. It combines user interaction with music knowledge, making it both fun and challenging.</p>
 
     <h2>How the game works</h2>
     <h3>Starting the game</h3>
     <ol>
-        <li>Users log in or register for the application</li>
-        <li>Users see a list of available players and invite them to play the game</li>
-        <li>Once the game is accepted, the game begins</li>
+        <li><strong>User Authentication:</strong> Users log in or register to access the game.</li>
+        <li><strong>Player Selection:</strong> Once logged in, users can view a list of available players and invite them to play.</li>
+        <li><strong>Game Initiation:</strong> When the invited player accepts the invitation, the game begins.</li>
     </ol>
 
-    <h3>The gameplay</h3>
+    <h3>The Gameplay</h3>
 
-    <p>A user starts by selecting an artist. The user enters an artist name. The application then returns (via an api) a
-        list of related artist.
-        The user selects the artist from this list.
-    </p>
+    <h4>1. Artist Selection</h4>
+
+    <ol>
+        <li>A user starts by entering the name of a musical artist.</li>
+        <li>The application retrieves a list of related artists using an external API.</li>
+        <li>The user selects the correct artist from this list to initiate the game.</li>
+    </ol>
 
     <img class="img-fluid" alt="SongArt" src="{{ asset('img/FirstArtistPick.png') }}">
 
-    <p>Players then go back and forth naming songs by the chosen artist.
-    </p>
+    <h4>2. Song Guessing</h4>
+
+    <ol>
+        <li>Players take turns guessing songs by the selected artist.</li>
+        <li>The application validates the guesses against the artist's discography.</li>
+        <li>Players earn points for each correct guess.</li>
+    </ol>
 
     <img class="img-fluid" alt="SongArt" src="{{ asset('img/GuessTheSong.png') }}">
 
-    <p>Once a player gets a song incorrect the opposing
-        player is given one more guess then a new artist is selected.</p>
+    <h4>3. Incorrect Guesses</h4>
+
+    <ol>
+        <li>If a player guesses a song incorrectly, the opposing player is given one more guess.</li>
+        <li>After this guess, a new artist is selected to continue the game.</li>
+    </ol>
 
     <img class="img-fluid" alt="SongArt" src="{{ asset('img/PickArtist.png') }}">
 
@@ -49,48 +60,63 @@
 
     <img class="img-fluid" alt="SongArt" src="{{ asset('img/GuessSongNew.png') }}">
 
+    <h3>Winning The Game</h3>
+    <ol>
+        <li>The game continues with players alternating turns and guessing songs.</li>
+        <li>The first player to reach 5 points wins the game.</li>
+    </ol>
 
     <h3>Technologies used</h3>
 
+    <h4>Frameworks and Libraries</h4>
+
     <ul>
-        <li>Laravel 8 and Vue2</li>
-        <li>Websockets</li>
-        <li>Apis</li>
+        <li><strong>PHP 8, Laravel 8 and Vue 2:</strong> The core of the application is built using PHP for the backend and Vue.js for the frontend.</li>
+        <li><strong>WebSockets:</strong> Utilized for real-time updates, ensuring users don't need to refresh pages between guesses.</li>
+        <li><strong>APIs:</strong>The Discogs API is integrated to verify artists, check songs, and return images of the selected musicians.</li>
     </ul>
 
-    <h3>Laravel and Vue</h3>
+    <h4>Deployment and Hosting</h4>
 
-    <p>The application uses Laravel, Vue and an SQL database. It is deployed to an Ubuntu server and is served with Nginx.
-    </p>
-    <p>Some basic checking on song guesses makes sure the song has not been guessed before and is similar to a song the
-        artist performs. I do this using the similar_text function in php:</p>
+    <ul>
+        <li><strong>Server:</strong> The application is deployed on an Ubuntu server.</li>
+        <li><strong>Web Server: </strong>Nginx is used to serve the application.</li>
+    </ul>
+
+    <h4>Song Guess Validation</h4>
+
+    <p>The application includes basic checks to ensure song guesses have not been previously used and are similar to the artist's actual songs. This is achieved using PHP’s similar_text function:</p>
 
     <pre><code>similar_text(strtolower($item["title"]), 
             strtolower($this->guess), 
             $perc); </code></pre>
 
-    <p> This allows for matches to be loosly set based on a defined matching percent.</p>
+    <p>This function allows for matches to be loosely set based on a defined matching percentage.</p>
 
-    <p>The data is stored using a relational database.</p>
+    <h4>Data Storage</h4>
+
+    <ul>
+        <li><strong>Database:</strong> A relational SQL database is used for storing all game data, including users, games, and song guesses.</li>
+    </ul>
 
     <img class="img-fluid" alt="SongArt" src="{{ asset('img/DatabaseStructure.png') }}">
 
+    <h4>Real-Time Communication</h4>
 
-    <h3>Websockets</h3>
+    <ul>
+        <li><strong>WebSockets:</strong> Laravel WebSockets package is employed to send real-time messages to the browser, allowing for seamless user experience without the need to refresh the page.</li>
+    </ul>
 
-    <p>Websockets are used so that users don't need to refresh pages between guesses. I used the <a
-            href="https://beyondco.de/docs/laravel-websockets/getting-started/introduction" target="_blank">Laravel
-            Websockets</a> package
-        to send messages to the browser. </p>
+    <h4>API Integration</h4>
+    <ul>
+        <li><strong>Discogs API:</strong>This <a href="https://www.discogs.com/developers" target="_blank"> API </a>  is used to verify artists, check song titles, and fetch images of the selected musicians, enhancing the game's functionality and user experience.</li>
+    </ul>
 
-    <h3>Api's</h3>
-
-    <p>The <a href="https://www.discogs.com/developers" target="_blank">Discog API </a> is used to
-        verfy
-        the artist, check the song and return a picture of the selected musician.</p>
-
-    <p>Check the code out on <a href="https://github.com/mttspear/laravel-running" target="_blank">Github</a> or check out
-        the application <a href="{{ route('login') }}"> Here </a> </p>
+    <h4>Check It Out</h4>
+    <ul>
+        <li><strong>Code Repository:</strong> The code is available on GitHub <a href="https://github.com/mttspear/laravel-running" target="_blank">here</a>. </li>
+        <li><strong>Live Application: </strong> Try out the application <a href="{{ route('login') }}"> here</a>.</li>
+    </ul>
 
     </div>
 @endsection
